beb06d5541bafbc9fe83b58ce3d36b1d9890a128
Vitor Machado Oliveira <vitor.oliveira@taqtile.com>
2015-07-08 10:49:27 -0300

    Adding cell for when choosing friend

---
+++ par-ou-impar/Components/ViewComponents/UITableViewCell/Friend/FriendTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FriendTableViewCell.swift
+//  par-ou-impar
+//
+//  Created by Taqtile on 7/8/15.
+//  Copyright (c) 2015 Viton. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var friendImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
