ce62266a660fd0364a623736167beb37b7e0777e
Vitor Machado Oliveira <vitor.oliveira@taqtile.com>
2015-07-15 14:15:00 -0300

    code verison

--- par-ou-impar/Modules/Home/HomeViewController.swift
+++ par-ou-impar/Modules/Home/HomeViewController.swift
@@ -80,13 +80,13 @@ extension HomeViewController: GameTableViewManagerDelegate {
     }
     
     func didSelectFinishGame(game:Game) {
-//        let fightViewController = FightViewController()
-//        fightViewController.game = game
-//        navigationController?.pushViewController(fightViewController, animated: true)
-
-        let fightViewController = MatchResultViewController()
+        let fightViewController = FightViewController()
         fightViewController.game = game
         navigationController?.pushViewController(fightViewController, animated: true)
+
+//        let fightViewController = MatchResultViewController()
+//        fightViewController.game = game
+//        navigationController?.pushViewController(fightViewController, animated: true)
     }
     
 }
