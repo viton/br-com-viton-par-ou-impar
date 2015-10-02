76d25cc963c8230909ee92385123797029f10a67
Vitor Machado Oliveira <vitor.oliveira@taqtile.com>
2015-10-02 14:21:20 -0300

    Brand new hands

--- par-ou-impar/Models/Provider/FightHandProvider/FightHandProvider.swift
+++ par-ou-impar/Models/Provider/FightHandProvider/FightHandProvider.swift
@@ -12,10 +12,14 @@ class FightHandProvider: NSObject {
    
     class func getHands() -> [FightHand] {
         var array = Array<FightHand>()
-        array.append(FightHand(name: "Branca", imagePrefix: "img_mao_normal_", handId: "normal_white"))
         array.append(FightHand(name: "Negra", imagePrefix: "img_mao_normal_negra_", handId: "normal_brown"))
         array.append(FightHand(name: "Rocker", imagePrefix: "img_mao_rocker_", handId: "rocker"))
+        array.append(FightHand(name: "Branca", imagePrefix: "img_mao_normal_", handId: "normal_white"))
+        array.append(FightHand(name: "Bluenails", imagePrefix: "img_mao_bluenails_", handId: "bluenails"))
+        array.append(FightHand(name: "Reggae", imagePrefix: "img_mao_reggae_", handId: "reggae"))
         array.append(FightHand(name: "Rednails", imagePrefix: "img_mao_rednails_", handId: "rednails"))
+        array.append(FightHand(name: "BlackSuit", imagePrefix: "img_mao_blacksuit_", handId: "blacksuit"))
+        array.append(FightHand(name: "WhiteSuit", imagePrefix: "img_mao_whitesuit_", handId: "whitesuit"))
         return array
     }
     
